ebed7b38f0905f016c8b8a43306771a993d84ce7
Soumya <soumyarelli16@gmail.com>
2023-08-29 16:51:29 +0530

    my first commit

--- app/src/main/java/com/example/myaadharscanner/IntroActivity.java
+++ app/src/main/java/com/example/myaadharscanner/IntroActivity.java
@@ -1,15 +1,12 @@
 package com.example.myaadharscanner;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-
-import com.example.myaadharscanner.adapters.IntroPagerAdapter;
 
 public class IntroActivity extends AppCompatActivity {
 
--- app/src/main/java/com/example/myaadharscanner/adapters/IntroPagerAdapter.java
+++
@@ -1,8 +0,0 @@
-package com.example.myaadharscanner.adapters;
-
-public class IntroPagerAdapter  {
-
-
-
-
-}
